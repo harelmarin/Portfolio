bb90e1de69391df1ef4b62bb1f753005f048df43
harelmarin <145020856+harelmarin@users.noreply.github.com>
2024-07-21 20:54:36 +0200

    add : header dynamic text

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+var options = {
+    strings: ["étudiant", "développeur", "designer", "passioné"],
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 2000,
+    loop: true
+  };
+  
+  var typed = new Typed(".dynamic-text", options);
