aa2520367bdc2fe0f24bc5597cdd357971b143a9
harelmarin <145020856+harelmarin@users.noreply.github.com>
2024-07-21 22:50:39 +0200

    Add : début section projet

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
+/* DYNAMIX TEXT */
+
 var options = {
-    strings: ["étudiant", "développeur", "designer", "passioné"],
+    strings: ["étudiant", "développeur", "designer", "passionné"],
     typeSpeed: 100,
     backSpeed: 50,
     backDelay: 2000,
@@ -7,3 +9,26 @@ var options = {
   };
   
   var typed = new Typed(".dynamic-text", options);
+
+
+  /* SCROLL */
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        } else {
+            entry.target.classList.remove("show");
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((element) => {
+    observer.observe(element);
+});
+
+
+/* LANDING PAGE */
+
