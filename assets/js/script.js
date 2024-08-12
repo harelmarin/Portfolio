4a8d73325a7f47c4703df55bff8b5b28e5be441c
harelmarin <145020856+harelmarin@users.noreply.github.com>
2024-08-12 17:59:50 +0200

    add : vidéo site vitrine

--- assets/js/script.js
+++ assets/js/script.js
@@ -58,7 +58,7 @@ document.addEventListener('DOMContentLoaded', function() {
         { id: '1', poster: 'assets/img/wethefoot.mp4' },
         { id: '2', poster: 'assets/img/BlogYmmersion.MP4' },
         { id: '3', poster: 'assets/img/Groupie.mp4' },
-        { id: '4', poster: 'assets/img/UBuscu.mp4' },
+        { id: '4', poster: 'assets/img/Crudapp.mp4' },
         { id: '5', poster: 'assets/img/Bookbox.mp4' },
       ];
 
